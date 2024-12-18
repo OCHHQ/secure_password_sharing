b5f2cdb367340ad58cd48b1c7ed9685e0eb51212
= <=>
2024-12-18 13:54:21 +0100

    Login page 100% responsive

--- secure_password_sharing-frontend/src/components/Login/Login.jsx
+++ secure_password_sharing-frontend/src/components/Login/Login.jsx
@@ -18,17 +18,17 @@ function Login() {
   };
 
   return (
-    <div className="border-4 w-3/4 rounded-3xl  shadow-2xl mx-auto my-20 p-10 lg:p-20 lg:w-1/2">
+    <div className="border-4 w-4/5 rounded-3xl  shadow-2xl mx-auto my-20 p-8 md:p-10  lg:w-1/2 ">
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 mb-3">
-          <h3 className="text-3xl sm:text-4xl lg:text-5xl font-semibold text-center">Welcome Back</h3>
-          <p className=" text-sm text-center font-medium lg:text-lg text-gray-500 mb-3">
+          <h3 className="text-2xl md:text-3xl text-center lg:text-5xl font-semibold">Welcome Back</h3>
+          <p className=" font-medium  text-center text-sm lg:text-lg text-gray-500 mb-4">
             Welcome back! Please enter your details
           </p>
         </div>
         <div className="flex flex-col gap-4">
           <div className="flex flex-col gap-2">
-            <label htmlFor="email" className="text-lg font-medium">
+            <label htmlFor="email" className="lg:text-lg text-base font-medium">
               Email:
             </label>
             <input
@@ -39,11 +39,11 @@ function Login() {
               onChange={handleChange}
               required
               placeholder="Enter your Email"
-              className="border-2 p-4 rounded-xl w-full border-gray-100 bg-transparent hover:border-gray-200 active:scale-[0.98]"
+              className="border-2 md:p-4 p-2 rounded-lg border-gray-400 placeholder:font-medium hover:border-indigo-600 focus:outline-indigo-600  active:scale-[0.98] bg-transparent"
             />
           </div>
           <div className="flex flex-col gap-2">
-            <label htmlFor="password" className="text-lg font-medium">
+            <label htmlFor="password" className="lg:text-lg text-base font-medium">
               Password:
             </label>
             <input
@@ -54,10 +54,10 @@ function Login() {
               onChange={handleChange}
               required
               placeholder="Enter your password"
-              className="border-2 p-4 rounded-xl w-full border-gray-100 bg-transparent hover:border-gray-200 active:scale-[0.98]"
+              className="border-2  md:p-4 p-2 rounded-lg border-gray-400 placeholder:font-medium hover:border-indigo-600 focus:outline-indigo-600  active:scale-[0.98] bg-transparent"
             />
           </div>
-          <div className="flex lg:flex-row justify-between lg:items-center mt-2">
+          <div className="flex flex-row justify-between lg:items-center mt-2">
             <label className="flex items-center text-base font-medium active:scale-[0.98]">
               <input
                 type="checkbox"
@@ -67,7 +67,7 @@ function Login() {
               Remember Me
             </label>
             <a
-              href="#"
+              href="/#Contact"
               className="font-medium text-base text-indigo-500 hover:underline"
             >
               Forgot Password?
@@ -75,12 +75,12 @@ function Login() {
           </div>
           <button
             type="submit"
-            className="active:scale-[.98] bg-indigo-600 text-white p-2 rounded-xl mt-4 hover:bg-indigo-500"
+            className="active:scale-[.98] bg-indigo-600 text-white p-2 rounded-xl mt-4 hover:bg-indigo-500 lg:text-xl md:text-lg text-base font-medium"
           >
             Login
           </button>
         </div>
-        <div className="flex justify-start gap-2 mt-4">
+        <div className="flex flex-row justify-between gap-2 mt-4">
           <p className="font-medium text-base">Don&apos;t have an account?</p>
           <a
             href="#"
