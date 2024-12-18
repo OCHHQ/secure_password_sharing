f90f2a8c77b636cb6f8b09d5eeb0dd1251da947d
= <=>
2024-12-18 13:54:54 +0100

    Signup page update

--- secure_password_sharing-frontend/src/components/Signup/Signup.jsx
+++ secure_password_sharing-frontend/src/components/Signup/Signup.jsx
@@ -23,7 +23,7 @@ function Signup() {
     // Add signup logic here
   };
   return (
-    <form onSubmit={handleSubmit} className="self-center w-4/5 md:w-3/4 border-4 rounded-3xl shadow-2xl mx-auto lg:my-20 my-32 p-8 md:p-10 lg:p-20  lg:w-1/2 bg- backdrop-blur-xl bg-[#F9F6F3]">
+    <form onSubmit={handleSubmit} className="self-center w-4/5 md:w-3/4 border-4 rounded-3xl shadow-2xl mx-auto lg:my-20 my-32 p-8 md:p-10  lg:w-1/2 bg- backdrop-blur-xl bg-white">
       <div className="flex flex-col gap-4 mb-3">
         <h3 className="text-2xl md:text-3xl text-center lg:text-5xl font-semibold">
           Create an Account
