451906263e1522c712c94590ff298c3982594668
= <=>
2024-12-28 10:20:58 +0100

    Update routage

--- secure_password_sharing-frontend/src/App.jsx
+++ secure_password_sharing-frontend/src/App.jsx
@@ -1,19 +1,29 @@
-import Login from "./components/Login/Login"
-import Home from  "./components/Home/Home"
-import Signup from "./components/Signup/Signup"
+import Login from "./components/Auth/Login"
+import Home from  "./components/Pages/Home"
+import Signup from "./components/Auth/Signup"
 import { BrowserRouter , Route, Routes } from "react-router-dom"
+import AppHome from "./components/Pages/AppHome"
+import VaultsPage from "./components/Pages/VaultsPage"
+import VaultsFolders from "./components/Pages/FoldersPage"
+import { UserProvider } from "./components/Context/UserContext"
+
 
 
 function App() {
   return (
     <>
+    <UserProvider>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/Login" element={<Login/>} />
+      <Route path="/Login" element={<Login />} />
       <Route path="/Signup" element={<Signup/>} />
+      <Route path="/Home" element={<AppHome/>} />
+      <Route path="/Vaults" element={<VaultsPage/>} />
+      <Route path="/Folders" element={<VaultsFolders/>} />
     </Routes>
     </BrowserRouter>
+    </UserProvider>
     </>
   )
 }
